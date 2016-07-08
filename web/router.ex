27a1ae541369bdae7738c078742f6c54835023bf
k.hamada <k.hamada.1988@gmail.com>
2016-07-09 00:47:05 +0900

    Init Phoenix

---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule Bookmarks.Router do
+  use Bookmarks.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Bookmarks do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Bookmarks do
+  #   pipe_through :api
+  # end
+end
