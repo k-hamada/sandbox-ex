7ccce9adcb73256096b4f66f7cd9844a150b933d
k.hamada <k.hamada.1988@gmail.com>
2016-07-23 23:54:55 +0900

    Change datetime format in view

--- filtration/web/views/entry_view.ex
+++ filtration/web/views/entry_view.ex
@@ -1,3 +1,9 @@
 defmodule Filtration.EntryView do
   use Filtration.Web, :view
+
+  def format_datetime(dt) do
+    dt
+    |> Calendar.DateTime.shift_zone!("Japan")
+    |> Calendar.Strftime.strftime!("%m/%d %H:%M")
+  end
 end
