4fffd31ebe6a34613d6e7fc4d1a135d7f4da1021
k.hamada <k.hamada.1988@gmail.com>
2016-11-26 01:19:14 +0900

    Add Snowflakex.Snowflakex.inspect

--- snowflakex/lib/snowflakex/snowflakex.ex
+++ snowflakex/lib/snowflakex/snowflakex.ex
@@ -25,4 +25,18 @@ defmodule Snowflakex.Snowflakex do
     DateTime.utc_now()
     |> DateTime.to_unix(:milliseconds)
   end
+
+  def inspect(id) do
+    {:ok, p_timestamp} =
+      id >>> (@maschine_id_bits + @sequence_bits)
+      |> Kernel.+(@epoch)
+      |> DateTime.from_unix(:milliseconds)
+    p_maschine_id =
+      id &&& generate(0, (1 <<< @maschine_id_bits + 1) -1, 0)
+    p_sequence =
+      id &&& generate(0, 0, (1 <<< @sequence_bits + 1) -1)
+
+    "timestamp: #{p_timestamp}\nmaschine_id: #{p_maschine_id}\nsequence: #{p_sequence}"
+    |> IO.puts
+  end
 end
